d301f9c0dea8e8b8e6caffa41f68727750ce7e58
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-14 22:09:37 +0300

    add public folder to basicServer

--- src/expressJs/basicServer/app.js
+++ src/expressJs/basicServer/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
     res.sendFile('./views/index.html',{root: __dirname})
 });
