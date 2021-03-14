fbcfd6565f7f29428e6558c0c4a92da598613005
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-14 13:02:58 +0300

    path module update

--- src/nodejs/path.js
+++ src/nodejs/path.js
@@ -1,13 +1,13 @@
-const path = require('path');   //node module
+const path = require("path"); //node module
 
-let result = path.resolve('path.js');   //file path
+let result = path.resolve("path.js"); // thisfile path
 
-result = path.extname('path.js');   //.js
+result = path.extname("path.js"); //.js
 
-result = path.parse(__filename);
+result = path.parse(__filename); // return object
 
-console.log(result.root);   // C:\
-console.log(result.ext);    // .js
-console.log(result.name);   // path
+console.log(result.root); // C:\
+console.log(result.ext); // .js
+console.log(result.name); // path
 
-// console.log(result);
+// console.log(result); 
