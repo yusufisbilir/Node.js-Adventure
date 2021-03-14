16862b44169c81ed356f49fb8b0a32039f31ea71
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-14 12:53:08 +0300

    added basic project

---
+++ src/expressJs/project/routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+//  /admin/add-product
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+});
+
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+});
+
+module.exports = router;
