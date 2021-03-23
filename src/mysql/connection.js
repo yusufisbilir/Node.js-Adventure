599e3540adc71f1c50933f5c669c8e0778b60e43
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-23 19:39:50 +0300

    create mysql table

--- src/mysql/connection.js
+++ src/mysql/connection.js
@@ -4,13 +4,20 @@ const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"1368",
+    database:"nodejsdb"
 });
 
 connection.connect((err)=>{
     if(err) console.log(err);
-    const createDB = "CREATE DATABASE nodejsDB"
-    connection.query(createDB, (err)=>{
-        if(err) throw err;
-        console.log("Created DB");
-    })
+
+    // const createDB = "CREATE DATABASE nodejsDB"
+    // connection.query(createDB, (err)=>{
+    //     if(err) throw err;
+    //     console.log("Created DB");
+    // })
+
+    connection.query("CREATE TABLE customers(name VARCHAR(30),adres VARCHAR(100))",(err)=>{
+        if(err) console.log(err);;
+        console.log('created table');
+    });
 });
